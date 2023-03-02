53e8985914e2bf29943388332fc57a005ff667e8
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-02 13:18:17 +0530

    added more items

--- src/Constant.js
+++ src/Constant.js
@@ -130,3 +130,162 @@ export const topSalesList = [
     },
   },
 ];
+
+export const formals = [
+  {
+    data: {
+      id: "1",
+      name: "Casual Big Size Men Shoes",
+      price: "3500",
+      image: [
+        "https://img.joomcdn.net/9ae9070997efd787c019358020694953aa3a0f82_original.jpeg",
+        "https://img.joomcdn.net/93a7489183b96c993f2be9873a7c13fd0466a7b9_original.jpeg",
+        "https://img.joomcdn.net/1f8f260972a21c4284f555941ee7a37ef3310f0d_original.jpeg",
+        "https://img.joomcdn.net/d39a30e6d22fdb0584b4c9f0c97df8aba6b6dfe9_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "2",
+      name: "Men Oxford Shoes Leather",
+      price: "4000",
+      image: [
+        "https://img.joomcdn.net/48dd4ce1c8a24a38b3f66dc7f446d39f30942496_original.jpeg",
+        "https://img.joomcdn.net/0689e9130eeddf51f6e8a1a106975c33f60731b5_original.jpeg",
+        "https://img.joomcdn.net/95b40012815bed290cb2b5b1b7920d5d8585620b_original.jpeg",
+        "https://img.joomcdn.net/06b2e57657022b13773a9ed081af533fef4f3620_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "3",
+      name: "Men's Leather Pointed Toe Business Shoes",
+      price: "2999",
+      image: [
+        "https://img.joomcdn.net/d2381f992880c422103a75975fa43dc2875010ec_original.jpeg",
+        "https://img.joomcdn.net/94cd847142047d7e80833b03b4016611279a3285_original.jpeg",
+        "https://img.joomcdn.net/786c15747797acaf6c62b1d9663ad09cdf54423f_original.jpeg",
+        "https://img.joomcdn.net/206afebff54c18b3583e7a78d5b451f26e66f547_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "4",
+      name: "New Men's Shoes Large Size Casual",
+      price: "1599",
+      image: [
+        "https://img.joomcdn.net/168bbd37ada5dbd7d970b0cefd8090da70119bb7_original.jpeg",
+        "https://img.joomcdn.net/49d0adfcb4b3d5ec3f6a055fb922e7637cf6277c_original.jpeg",
+        "https://img.joomcdn.net/466b31b52a981a628cf73a753d8060c0a6dc48e5_original.jpeg",
+        "https://img.joomcdn.net/59392c5cb1403b20f8cfe7fab0e26bcc680eab5c_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "5",
+      name: "Leather Shoes Genuine Leather England",
+      price: "9999",
+      image: [
+        "https://img.joomcdn.net/45ea1be4a18b55b545e5e0a803db47071e393c66_original.jpeg",
+        "https://img.joomcdn.net/b418343d655fb9bed4a54858a6c40e660a9f551a_original.jpeg",
+        "https://img.joomcdn.net/27e9f97182af3dff75eeedc4a57950762df01cbd_original.jpeg",
+        "https://img.joomcdn.net/fa3edfd5f65ee0eeedd920a3f2b95f945cf3952e_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "6",
+      name: "Casual Shoes Men's Patent Leather Zipper Decorative",
+      price: "7999",
+      image: [
+        "https://img.joomcdn.net/627c5f89829854cbc2de30a45bf3b37113decf67_original.jpeg",
+        "https://img.joomcdn.net/bea7653c45a74a2fc2d4be5a3936cd632415b793_original.jpeg",
+        "https://img.joomcdn.net/84b330f131fe00bf753139f376a5da10384ece14_original.jpeg",
+        "https://img.joomcdn.net/0f2781ed9b1e9561b1aa3b3c9746d29f3aa9f838_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "6",
+      name: "Casual Shoes Men's Patent Leather Zipper Decorative",
+      price: "7999",
+      image: [
+        "https://img.joomcdn.net/627c5f89829854cbc2de30a45bf3b37113decf67_original.jpeg",
+        "https://img.joomcdn.net/bea7653c45a74a2fc2d4be5a3936cd632415b793_original.jpeg",
+        "https://img.joomcdn.net/84b330f131fe00bf753139f376a5da10384ece14_original.jpeg",
+        "https://img.joomcdn.net/0f2781ed9b1e9561b1aa3b3c9746d29f3aa9f838_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "7",
+      name: "Black Men's Business Leather Shoes Breathable",
+      price: "3999",
+      image: [
+        "https://img.joomcdn.net/16825c226298e52540d24ccba05c26c071f48624_original.jpeg",
+        "https://img.joomcdn.net/16825c226298e52540d24ccba05c26c071f48624_original.jpeg",
+        "https://img.joomcdn.net/e2c282122e8f5f7aed839e859f9d3ad06d7df998_original.jpeg",
+        "https://img.joomcdn.net/807997075a2730d2160e7e3ee73fa2c056b6b9e1_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "8",
+      name: "New Spring and Autumn Men's Lace Up Leather Shoes",
+      price: "3599",
+      image: [
+        "https://img.joomcdn.net/1b360b42e758385ffdd155d2ea2941b35845418f_original.jpeg",
+        "https://img.joomcdn.net/58780c31904d5028dcddfbbb6382e1a475bedd6d_original.jpeg",
+        "https://img.joomcdn.net/f77fdc13b13439a8469006523f0d4e898746971a_original.jpeg",
+        "https://img.joomcdn.net/1a61a8db921e86e68cc3452df775b6a96ba18a26_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "9",
+      name: "New Business Casual Men's Leather Shoes Soft Bottom",
+      price: "3199",
+      image: [
+        "https://img.joomcdn.net/2fcf6479d0312738f20fd29330828dc55a19c55e_original.jpeg",
+        "https://img.joomcdn.net/0a90994dd4b89498651d43aabe3c538a963aee65_original.jpeg",
+        "https://img.joomcdn.net/8f5e61aa0bcb1206a1774a3ea17ea427f82b3ccf_original.jpeg",
+        "https://img.joomcdn.net/ca46d91936e5d3e9dc1f55a364f054b38e8714ad_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "10",
+      name: "Shoes Konors 1164 40 Black Leather",
+      price: "10199",
+      image: [
+        "https://img.joomcdn.net/a4df368791fa7bc6cac415564726de0f522e2dea_1024_680.jpeg",
+        "https://img.joomcdn.net/71acb5d31d5fdff350cd29a2641a068c0d9f4f05_1024_680.jpeg",
+        "https://img.joomcdn.net/13f83089db188ef3fff13b76022fdc8c1534dd0c_1024_680.jpeg",
+        "https://img.joomcdn.net/e9aa98a97f75e2c6c31a20529e910c524d545729_1024_680.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "11",
+      name: "Slip-ons Ikos 33671 40 Black Leather",
+      price: "8560",
+      image: [
+        "https://img.joomcdn.net/059ecbee90682738e3810a59776a4df6dd0b688e_1024_680.jpeg",
+        "https://img.joomcdn.net/d5ff7d814d890ee1e558bd8648e49327f849318a_1024_680.jpeg",
+        "https://img.joomcdn.net/6ec00f17b5cb0ee02bbaa2b963d17afffbec1b82_1024_680.jpeg",
+        "https://img.joomcdn.net/25220b2667466406e674306910c59ba251ebfa94_1024_690.jpeg",
+      ],
+    },
+  },
+];
