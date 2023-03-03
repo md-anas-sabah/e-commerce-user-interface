7b5a612093b8abe888c3db2277dbe937fbeed38d
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-04 01:07:42 +0530

    added items

--- src/components/AthleisureCategory.jsx
+++ src/components/AthleisureCategory.jsx
@@ -4,12 +4,12 @@ const AthleisureCategory = () => {
   return (
     <div className="h-[45rem] w-[24rem] relative">
       <img
-        src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRfhvuA6nrtB7FjqhlKCTmfKhE_pkKvR5OClPHcAtVl2dwLa4LciT6AY5iC-NeE9gRR_8qmdJe3IcP6eLZYKrtfDA7I7kjsKg&usqp=CAE"
+        src="https://i.pinimg.com/736x/aa/30/58/aa3058c59b66a788b6b2bfe4d3ecb656.jpg"
         alt="formal-logo"
         className="absolute inset-0 bg-cover bg-center z-0 h-[45rem] w-[24rem] object-cover rounded-2xl"
       />
       <h1 className="opacity-0 hover:opacity-70 bg-black rounded-2xl duration-300 absolute inset-0 z-10 flex justify-center  border  items-center text-6xl text-white font-semibold font-bebas tracking-wider">
-        Athleisure
+        Boots
       </h1>
     </div>
   );
