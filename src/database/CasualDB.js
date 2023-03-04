dae66b3a1e01783435513f350a0ce27280cc9320
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-04 13:39:58 +0530

    added items

--- src/database/CasualDB.js
+++ src/database/CasualDB.js
@@ -38,4 +38,121 @@ export const casual = [
       ],
     },
   },
+  {
+    data: {
+      id: "4",
+      name: "Nike Air Max SC - Mens Shoes Black CW4555-002 Sneakers Sport Shoes ORIGINAL",
+      price: "4200",
+      image: [
+        "https://img.joomcdn.net/92bf131aa918d8937cb2355b480fed2d1c1c5a01_1024_909.jpeg",
+        "https://img.joomcdn.net/52d2ae3790f014f7455f2dda88594e46acd3e14b_1024_909.jpeg",
+        "https://img.joomcdn.net/6c7eb94017d278fd289b6b15f49485345eb09410_1024_909.jpeg",
+        "https://img.joomcdn.net/7594df5d9423ce80d3ec052963277d8cc3a366be_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "5",
+      name: "Men Sneakers Running Walking Shoes Casual Breathable Flat Air Mesh Lace Up Shoes",
+      price: "1100",
+      image: [
+        "https://img.joomcdn.net/b71e4e4b98996aadd247f1991d2ee2a8bac4ca9e_original.jpeg",
+        "https://img.joomcdn.net/a2676b648ad4d379e7e873d09cfb2be6aad5acc9_original.jpeg",
+        "https://img.joomcdn.net/3b20a094502a3a25735594e7179d64fcbf07fb7b_original.jpeg",
+        "https://img.joomcdn.net/403fe62c02d781f1bccab65da1a82f84f023878c_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "6",
+      name: "Adidas X Quesence X Solebox - Italian Leathers - Mens Shoes Leather Clear-White DB1785 Sneakers Sports Shoes ORIGINAL",
+      price: "7860",
+      image: [
+        "https://img.joomcdn.net/df04e4ab55da04c30c22dd6f84bc6d8cd3abc6e9_1024_909.jpeg",
+        "https://img.joomcdn.net/ac15e6f6a46b685831a75a4905bca2d84bd68b48_1024_909.jpeg",
+        "https://img.joomcdn.net/b799882e3d7bf7226f17c38ad6167c58b6e94ba9_1024_909.jpeg",
+        "https://img.joomcdn.net/2deaad85217c68dc902d3a1d5310ee7cb63240eb_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "7",
+      name: "JORDAN 1 Low FlyEase - Men's Shoes Leather White-Red DM1206-163 Sneakers Sport Shoes ORIGINAL",
+      price: "10999",
+      image: [
+        "https://img.joomcdn.net/55fc377aa3ff465f44f46ddd23238365cff1c9a9_1024_909.jpeg",
+        "https://img.joomcdn.net/49eee021f79bb24bef6b92af5b7656670bda71c1_1024_909.jpeg ",
+        "https://img.joomcdn.net/49eee021f79bb24bef6b92af5b7656670bda71c1_1024_909.jpeg",
+        "https://img.joomcdn.net/262bfed03c01659f3234b5e21f83ef7a2238639c_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "8",
+      name: "Adidas Originals OZELIA - Men's Sneakers Black H04250 Retro Shoes Sports Shoes ORIGINAL",
+      price: "4599",
+      image: [
+        "https://img.joomcdn.net/3c4558de27c81a5b915f213e25697d1f34bff1a0_1024_909.jpeg",
+        "https://img.joomcdn.net/07f9a50b3392c9600e3f04fc18935132fdaf13fd_1024_909.jpeg ",
+        "https://img.joomcdn.net/a3a92463e88fe9c0214ba33135f33aa29486b1d6_1024_909.jpeg",
+        "https://img.joomcdn.net/abc7bce5ccd9c793406b51c290c7b1abd56a34a7_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "9",
+      name: "ONEMIX High Top Men Running Shoes Waterproof Sneakers For Outdoor Sports Walking Jogging Treckking Shoes",
+      price: "3540",
+      image: [
+        "https://img.joomcdn.net/7a11aca3b528e1d1d29ddcddb5845f0c21c0027e_1024_1024.jpeg",
+        "https://img.joomcdn.net/a2b3e88ff2d491d147326c9d5076f10e6446b353_original.jpeg ",
+        "https://img.joomcdn.net/a2b3e88ff2d491d147326c9d5076f10e6446b353_original.jpeg",
+        "https://img.joomcdn.net/ce5e570049ea933990e335d92a8f25b2a5f6326b_original.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "10",
+      name: "Nike Air Max Terrascape Plus TN - Mens Shoes White DN4590-100 Sneakers Sport Shoes ORIGINAL",
+      price: "6599",
+      image: [
+        "https://img.joomcdn.net/f02bc3f511f1b4cc766dd647a1d6219802d29b1d_1024_909.jpeg",
+        "https://img.joomcdn.net/7d492b8b926b82ba5fb7a60555c9e6b16838cb06_1024_909.jpeg",
+        "https://img.joomcdn.net/7d492b8b926b82ba5fb7a60555c9e6b16838cb06_1024_909.jpeg",
+        "https://img.joomcdn.net/e1e250511ecf535efc9d0ac0c35b00b7c7904920_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "11",
+      name: "PUMA Caven - Men's Leisure Shoes White 380810-02 Sneakers Sport Shoes ORIGINAL",
+      price: "2999",
+      image: [
+        "https://img.joomcdn.net/a97ee87ace39851133ba54bb70c0d102dce124c5_1024_909.jpeg",
+        "https://img.joomcdn.net/a97ee87ace39851133ba54bb70c0d102dce124c5_1024_909.jpeg",
+        "https://img.joomcdn.net/02fce836d50f591bf00c585fdf2808d60b4a6360_1024_909.jpeg",
+        "https://img.joomcdn.net/43297a7a91064ce72e470da071ea67f7d6f0d978_1024_909.jpeg",
+      ],
+    },
+  },
+  {
+    data: {
+      id: "12",
+      name: "New Spring and Summer Men's Sneakers Lightweight Breathable Non-slip Men's Plimsolls Running Canvas Shoes",
+      price: "849",
+      image: [
+        "https://img.joomcdn.net/0807e3646a2ea082b2908d2eee36d733c3d3af13_original.jpeg",
+        "https://img.joomcdn.net/b8b09d4b4449da442b9a6991a36474737cd28bf9_original.jpeg",
+        "https://img.joomcdn.net/cf5cd609b3485054dbeec1e8d178ac8ede021e4f_original.jpeg",
+        "https://img.joomcdn.net/587617e5c59e6161de0fc473f850f46c4a11ef6b_original.jpeg",
+      ],
+    },
+  },
 ];
