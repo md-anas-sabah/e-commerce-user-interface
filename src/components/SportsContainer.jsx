c2031e0609ec61a6b0b4dc654a2e2ff99dc791de
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-04 15:40:20 +0530

    added items

---
+++ src/components/SportsContainer.jsx
@@ -0,0 +1,24 @@
+import { casual } from "../database/CasualDB";
+import Card from "./Card";
+
+const SportsContainer = () => {
+  return (
+    <>
+      <div>
+        <h1 className="font-bebas font-extrabold text-5xl">
+          Sneakers & Low-tops
+        </h1>
+      </div>
+      <div className="flex flex-wrap gap-12 p-7 ml-10">
+        {casual.map((c) => {
+          return (
+            <>
+              <Card {...c.data} key={c.data.id} />
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default SportsContainer;
---
+++ src/database/SportsDB.js
@@ -0,0 +1,35 @@
+export const Sports = [
+  {
+    id: "1",
+    name: "28-38 High Quality Teen Fashion Anti-slip Wear-resistant Rubber Bottom Football Shoes Children's Football Shoes",
+    price: "4494",
+    image: [
+      "https://img.joomcdn.net/0558c34842b3b3dc6b346b9eb60b5ad89c55c3c9_original.jpeg",
+      "https://img.joomcdn.net/f916601243ea4f764a48750dc345a8ad5aaf5658_original.jpeg",
+      "https://img.joomcdn.net/eea5a4e1d165f2bd855664ad5791ea0ff647729f_original.jpeg",
+      "https://img.joomcdn.net/3bf944f94067d09a5f713fa962a7981ea72204d3_original.jpeg",
+    ],
+  },
+  {
+    id: "2",
+    name: "28-39 Children's High-quality Rotating Buttons Football Football Children's Patent Leather Art Format Lawn Training Football Boots",
+    price: "6945",
+    image: [
+      "https://img.joomcdn.net/0ab26a9ef55598c480a25d3147ed6f5c46af8930_original.jpeg",
+      "https://img.joomcdn.net/444eeaf5d714d805823c730e8f4a6d5d9de029b9_original.jpeg",
+      "https://img.joomcdn.net/73f5ff4e1e5bb936e36f0a8f9a834d057a2db627_original.jpeg",
+      "https://img.joomcdn.net/38a69bb72b13445ba890811204244824eba13353_original.jpeg",
+    ],
+  },
+  {
+    id: "3",
+    name: "Men's High Top Professional Competition Long Spike Sports Football Shoes High Quality Comfortable Men's Soccer Shoes",
+    price: "7548",
+    image: [
+      "https://img.joomcdn.net/24dcf7d2fbc9b8662630ff1af1c79b8ea6724841_original.jpeg",
+      "https://img.joomcdn.net/3b84ce35f0119bfba97a149d08051abd622ae7c9_original.jpeg",
+      "https://img.joomcdn.net/b0402b0b0de3e27fa2fea96457cc73497dee911d_original.jpeg",
+      "https://img.joomcdn.net/85f73002150a35fb14978380fe0fa0a57162c79d_original.jpeg",
+    ],
+  },
+];
