8dcae081fd66e049071cb87d1a28234db61624b4
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-07 21:11:38 +0530

    updated: destructuring the store here

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,25 +2,23 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CheckoutCard from "../components/CheckoutCard";
 import { closeCart } from "../redux/toggleCartSlice";
-import useTotalPrice from "../Utils/useTotalPrice";
 
 const Checkout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeCart());
   });
-  const cartItem = useSelector((store) => store.cart.items);
-  console.log(cartItem);
+  const { items, amount, total } = useSelector((store) => store.cart);
 
   return (
     <div className="flex flex-col mt-14 w-10/12 ml-auto mr-auto border shadow-2xl">
       <div className="flex flex-col items-center gap-3 ">
-        {cartItem.map((item) => {
+        {items.map((item) => {
           return <CheckoutCard {...item.data} key={item.data.id} />;
         })}
       </div>
       <div className="">
-        <h1>Total:</h1>
+        <h1>Total: ₹{total} </h1>
       </div>
     </div>
   );
