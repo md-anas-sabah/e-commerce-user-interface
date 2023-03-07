46b63f14c039b690c0e564bfc27f431440d38295
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-07 21:14:09 +0530

    updated: added removeItem functionality

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -4,12 +4,17 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
+    amount: 1,
+    total: 0,
   },
   reducers: {
     addItem: (state, action) => {
       state.items.push(action.payload);
     },
-    // removeItem: (state, action) => {},
+    removeItem: (state, action) => {
+      const itemID = action.payload;
+      state.items = state.items.filter((item) => item.id !== itemID);
+    },
   },
 });
 
